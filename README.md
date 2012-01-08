3c904f9a2224375bd104bf15d73415cbbdf4b6e7
jbroadway <johnny@johnnybroadway.com>
2012-01-08 14:56:30 -0600

    Added LevelBuffer handler

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ examples for each in the examples folder. These include:
 * Buffer - Buffer messages to send all at once (works with Mail handler)
 * File - Append messages to a file
 * FirePHP - Send messages to [FirePHP](http://www.firephp.org/) browser plugin
+* LevelBuffer - Buffer messages and send only if sufficient error level reached
 * Mail - Send email notices
 * Mongo - Save to MongoDB collection
 * Multi - Send different log levels to different handlers
---
+++ examples/levelbuffer.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'autoload.php';
+
+Analog::handler (Analog\Handler\LevelBuffer::init (
+	Analog\Handler\Mail::init (
+		'john.luxford@gmail.com',
+		'Attention: A critical error has occurred',
+		'noreply@example.com'
+	),
+	Analog::CRITICAL
+));
+
+// none of these will trigger sending the log
+Analog::log ('Debugging...', Analog::DEBUG);
+Analog::log ('Minor warning...', Analog::WARNING);
+Analog::log ('An error...', Analog::ERROR);
+
+// but this will, and will include all the others in the log
+Analog::log ('Oh noes!', Analog::URGENT);
+
+?>
