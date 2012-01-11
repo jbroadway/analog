f45c793e0eac35b3b0d284bbedb6550c91d07ee0
jbroadway <johnny@johnnybroadway.com>
2012-01-10 23:17:18 -0600

    Updated Analog core to provide its own autoloader for the pre-built handlers

--- Analog.php
+++ Analog.php
@@ -170,3 +170,16 @@ class Analog {
 		return self::write (self::get_struct ($message, $level));
 	}
 }
+
+/**
+ * Register a very simple autoloader for the pre-built handlers
+ * based on the current working directory.
+ */
+spl_autoload_register (function ($class) {
+	$file = str_replace ('\\', DIRECTORY_SEPARATOR, ltrim ($class, '\\')) . '.php';
+	if (file_exists (__DIR__ . DIRECTORY_SEPARATOR . $file)) {
+		require_once $file;
+		return true;
+	}
+	return false;
+});
