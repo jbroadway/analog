7969439a2e0eca333a75c124a17a11d8e188c7b5
Johnny Broadway <johnny@johnnybroadway.com>
2012-02-23 12:08:18 -0600

    Updated tests to ensure they start with a fresh file

--- tests/AnalogTest.php
+++ tests/AnalogTest.php
@@ -6,6 +6,8 @@ class AnalogTest extends PHPUnit_Framework_TestCase {
 	public static $log = '';
 
 	function test_default () {
+		@unlink (Analog::handler ());
+
 		// Check it wrote correctly to temp file
 		Analog::log ('Foo');
 		$this->assertEquals (
@@ -15,6 +17,9 @@ class AnalogTest extends PHPUnit_Framework_TestCase {
 		unlink (Analog::handler ());
 	}
 
+	/**
+	 * @depends test_default
+	 */
 	function test_format () {
 		// Test changing the format string and write again
 		Analog::$format = "%s, %s, %d, %s\n";
@@ -26,6 +31,9 @@ class AnalogTest extends PHPUnit_Framework_TestCase {
 		unlink (Analog::handler ());
 	}
 
+	/**
+	 * @depends test_format
+	 */
 	function test_handler () {
 		// Test logging using a closure
 		Analog::handler (function ($msg) {
