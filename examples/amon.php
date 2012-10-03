cc76d7e4fff9c1e99a87c5dc408c00547f98ec1f
Johnny Broadway <johnny@johnnybroadway.com>
2012-10-03 09:42:31 -0500

    Updated Amon example

--- examples/amon.php
+++ examples/amon.php
@@ -3,6 +3,8 @@
 // 1. Install the Amon PHP lib from http://amon.cx/guide/clients/php/
 require 'amon.php';
 
+require '../lib/Analog.php';
+
 Analog::handler (Analog\Handler\Amon::init (
 	'http://127.0.0.1',
 	2464
